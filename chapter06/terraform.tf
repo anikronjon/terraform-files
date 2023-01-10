f9ab1b90d9029fe34df993932d4edaa3e7e2a8e9
AnikRonjon <anik35-497@diu.edu.bd>
2023-01-10 21:50:23 +0800

    github repository automate using tf

---
+++ chapter06/terraform.tf
@@ -0,0 +1,21 @@
+
+# github provider
+provider "github" {
+  token = var.token
+}
+
+resource "github_repository" "frontend" {
+  name        = "frontend"
+  description = "My react frontend"
+  visibility = "public"
+  auto_init = true
+}
+
+
+resource "github_repository" "backend" {
+  name        = "backend"
+  description = "My django backend"
+  visibility = "public"
+  auto_init = true
+}
+
